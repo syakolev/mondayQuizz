13888d04a96533a67852caa59bc3f53c598fd8a4
syakolev <syakolev@gmail.com>
2022-04-12 01:22:22 +0300

    second

--- src/context.js
+++ src/context.js
@@ -6,14 +6,40 @@ const url = 'https://opentdb.com/api.php?amount=100'
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-    const [waiting, setWaiting] = useState(false)
-    const [loading, setLoading] = useState(false)
+    const [waiting, setWaiting] = useState(true)
+    const [loading, setLoading] = useState(true)
     const [questions, setQuestions] = useState([])
     const [index, setIndex] = useState(0)
     const [correct, setCorrect] = useState(0)
     const [error, setError] = useState(false)
     const [isModalOpen, setIsModalOpen] = useState(false)
 
+    const fetchQuestions = async (url) => {
+        setLoading(true)
+        setWaiting(false)
+        const response = await axios(url).catch((err) => console.log(err))
+        console.log(response)
+        if (response) {
+            const data = response.data.results
+            if (data.length > 0) {
+                setQuestions(data)
+                setLoading(false)
+                setWaiting(false)
+                setError(false)
+            } 
+            else {
+                setWaiting(true)
+                setError(true)
+            }
+        } 
+        else {
+            setWaiting(true)
+        }
+    }
+
+    useEffect(() => {
+        fetchQuestions(url)
+    },[])
 return (
     <AppContext.Provider
         value={{
