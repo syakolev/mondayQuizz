5dcab66e8d37006c9f23d1b76631bd2bac3bedb1
syakolev <syakolev@gmail.com>
2022-04-11 19:42:35 +0300

    start

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-name = 'Saar'
-
-alert(name)
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
